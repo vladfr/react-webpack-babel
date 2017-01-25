462ed2090358b1963d673f2294f5085ff0e2cd0b
Ali Al Dallal <ali@alicoding.com>
2017-01-25 08:52:05 -0500

    Example for bootstrap4

--- src/app.jsx
+++ src/app.jsx
@@ -7,6 +7,12 @@ export default class App extends React.Component {
         <h1>It Works!</h1>
         <p>This React project just works including <span className="redBg">module</span> local styles.</p>
         <p>Enjoy!</p>
+        <div>
+        	<h1 className="display-1">Display 1</h1>
+        	<h1 className="display-2">Display 2</h1>
+        	<h1 className="display-3">Display 3</h1>
+        	<h1 className="display-4">Display 4</h1>
+        </div>
       </div>
     )
   }
